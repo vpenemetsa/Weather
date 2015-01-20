f74bf6646f898e8e64973ac3b7264eac71935e6b
Vijay Penemetsa <vpenemetsa@sailthru.com>
2015-01-19 19:25:26 -0500

    Added location detail layout

--- src/main/java/org/vpenemetsa/oneplusweather/activities/DetailActivity.java
+++ src/main/java/org/vpenemetsa/oneplusweather/activities/DetailActivity.java
@@ -20,6 +20,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.vpenemetsa.oneplusweather.Constants;
@@ -124,11 +125,6 @@ public class DetailActivity extends FragmentActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
 
         /**
          * Returns a new instance of this fragment for the given section
@@ -137,7 +133,6 @@ public class DetailActivity extends FragmentActivity {
         public static PlaceholderFragment newInstance(int sectionNumber) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
             fragment.setArguments(args);
             return fragment;
         }
@@ -149,6 +144,9 @@ public class DetailActivity extends FragmentActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            ImageView
+
             return rootView;
         }
     }
